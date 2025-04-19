6428968de35ab0a0559feb92a0b5c4e516ef0f19
MostafaGamal7 <drshmostafa62@gmail.com>
2025-04-19 15:12:49 +0200

    Fixed Token

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,8 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabaseUrl = "https://xckmclmqaqrgcxhdozzu.supabase.co";
-const supabase = createClient(
-  supabaseUrl,
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhja21jbG1xYXFyZ2N4aGRvenp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk1MDU3MTQsImV4cCI6MjAxNTA4MTcxNH0.BQusEUVLiF8xXSJqkNNPztu8ssnFV_X1Zqy04jH1o_4"
-);
+const supabase = createClient(supabaseUrl, process.env.SUPABSE_TOKEN);
 export default supabase;

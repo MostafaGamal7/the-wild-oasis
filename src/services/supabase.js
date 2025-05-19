112ca072e301f07448320d6643b2bbd3457f5e65
MostafaGamal7 <drshmostafa62@gmail.com>
2025-05-19 05:10:56 +0300

    changed database

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,5 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabaseUrl = "https://xckmclmqaqrgcxhdozzu.supabase.co";
-const supabase = createClient(supabaseUrl, process.env.SUPABSE_TOKEN);
+// const supabase = createClient(supabaseUrl, process.env.SUPABSE_TOKEN);
+const supabaseKey = import.meta.env.VITE_SUPABASE_TOKEN;
+const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;

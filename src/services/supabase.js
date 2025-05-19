466503e88fc911b16bbb245a46a61fc6f844d60c
MostafaGamal7 <drshmostafa62@gmail.com>
2025-05-19 05:19:28 +0300

    changed database

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+// OLD
 // Create a single supabase client for interacting with your database
-export const supabaseUrl = "https://xckmclmqaqrgcxhdozzu.supabase.co";
+// export const supabaseUrl = "https://xckmclmqaqrgcxhdozzu.supabase.co";
 // const supabase = createClient(supabaseUrl, process.env.SUPABSE_TOKEN);
+
+export const supabaseUrl = "https://gadcehaddekxmslafamf.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_TOKEN;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
